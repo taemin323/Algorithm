d9a02f1e478790713e78168325ffd9561c3f71b4
taemin323 <155932839+taemin323@users.noreply.github.com>
2024-12-17 21:11:38 +0900

    [Silver III] Title: N과 M (7), Time: 560 ms, Memory: 123340 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int N;
+	private static int M;
+	private static StringBuilder sb;
+	private static int[] arr;
+	private static int[] selected;
+	private static boolean[] visited;
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		arr = new int[N];
+		selected = new int[M];
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		perm(0);
+		
+		System.out.println(sb.toString());
+	}//end of main
+
+	private static void perm(int cnt) {
+		if(cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(selected[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for (int i = 0; i < N; i++) {
+			selected[cnt] = arr[i];
+			perm(cnt+1);
+		}
+	}
+}//end of class
