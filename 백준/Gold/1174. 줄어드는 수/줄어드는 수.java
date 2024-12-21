6dca97e39be3affa2ad9f83f01c26c738eb27291
taemin323 <155932839+taemin323@users.noreply.github.com>
2024-12-21 13:54:10 +0900

    [Gold V] Title: 줄어드는 수, Time: 128 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.java
@@ -5,37 +5,38 @@ import java.util.List;
 
 public class Main {
 	 
-    static int n;
+	private static int N;
     static int [] arr = {9,8,7,6,5,4,3,2,1,0};
     static List<Long> list = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
  
-        n = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
  
         dfs(0,0);
         list.sort(null);
  
  
-        try{
-            System.out.println(list.get(n-1));
-        }catch (Exception e){
-            System.out.println(-1);
+        if(N > list.size()) {
+        	System.out.println(-1);
+        } else {
+        	System.out.println(list.get(N-1));
         }
- 
     }
  
-    private static void dfs(long num, int index){
-        if(!list.contains(num)){
-            list.add(num);
+    private static void dfs(long num, int idx){
+        if(idx == 10) {
+        	if(!list.contains(num)) {
+        		list.add(num);
+        	}
+        	return;
         }
- 
-        if(index>=10){
-            return;
-        }
- 
-        dfs((num*10)+arr[index], index+1);
-        dfs(num,index+1);
+    	
+        // 해당 인덱스를 선택 안하고 넘어가.
+        dfs((num*10)+arr[idx], idx+1);
+
+        // 해당 인덱스를 선택한 경우 num에 더해준다.
+        dfs(num,idx+1);
  
     }//end of main
 }//end of class
