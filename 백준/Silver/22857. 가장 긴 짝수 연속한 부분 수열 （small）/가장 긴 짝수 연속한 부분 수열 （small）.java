796f406657722e90d4fc2c52cf134fa7a4aa7a7c
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-01-02 15:27:22 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 232 ms, Memory: 21712 KB -BaekjoonHub

--- 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.java
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.java
@@ -10,11 +10,7 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
 		
-		int[] arr = new int[N];
-		
-		//dp[i][j] = 처음부터 i번째 원소까지 고려했을 때, 최대 j개의 홀수를 제거하여 얻을 수 있는
-		//가장 긴 연속된 짝수 부분 수열의 길이
-		int[][] dp = new int[N+1][K+1];
+		int[] arr = new int[N+1];
 		
 		st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < N; i++) {
@@ -22,18 +18,25 @@ public class Main {
 		}// 입력 완료
 		
 		int max = 0;
-		for (int i = 1; i <= N; i++) {
-			for (int j = 0; j <= K; j++) {
-				if(arr[i-1] % 2 == 0) {
-					dp[i][j] = dp[i-1][j] + 1;
-				} else {
-					if(j>0) {
-						dp[i][j] = dp[i-1][j-1];
-					}
-				}
-				max = Math.max(max, dp[i][j]);
+		int left = 0;
+		int oddCnt = 0;//제거한 홀수 개수
+		
+		//투포인터
+		for (int right = 0; right < N; right++) {
+			if(arr[right] % 2 != 0) {
+				oddCnt++;
 			}
+			
+			while(oddCnt > K) {
+				if(arr[left] % 2 != 0) {
+					oddCnt--;
+				}
+				left++;
+			}
+			max = Math.max(max, right-left+1-oddCnt);
 		}
+		
 		System.out.println(max);
+	
 	}//end of main
 }//end of class
