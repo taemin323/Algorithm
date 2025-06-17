538c9668afc5e19c5b4b63db3b53416b2d9bbc13
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-06-17 12:28:04 +0900

    [level 1] Title: K번째수, Time: 0.02 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -14,10 +14,23 @@ class Solution {
                 command[j-start] = array[j];
             }
             
-            Arrays.sort(command);
+            // Arrays.sort(command);
+            bubbleSort(command);
             
             answer[i] = command[target];
         }
         return answer;
+    }
+    
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 1; j < arr.length -i; j++) {
+                if(arr[j] < arr[j-1]) {
+                    int tmp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
     }
 }
