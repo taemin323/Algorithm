b064b32251afdfd110cc8123ba4a864bac38dcd0
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-07-30 15:27:23 +0900

    [Gold V] Title: 탑, Time: 2560 ms, Memory: 134020 KB -BaekjoonHub

---
+++ 백준/Gold/2493. 탑/탑.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * 1 <= N <= 500,000
+ *  1 <= top <= 100,000,000 
+ */
+
+public class Main {
+	
+	static class Top {
+		int h, idx;
+		
+		public Top() {
+		}
+
+		public Top(int h, int idx) {
+			super();
+			this.h = h;
+			this.idx = idx;
+		}
+		
+	}
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int[] tops = new int[N];
+		int[] ans = new int[N];
+		ans[0] = 0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for(int i = 0; i < N; i++) {
+			tops[i] = Integer.parseInt(st.nextToken());
+		}// 입력 완료
+		
+		
+		Stack<Top> stack = new Stack<>();
+		stack.add(new Top(tops[0], 0));
+		
+		for (int i = 1; i < N; i++) {
+			
+			while (!stack.isEmpty() && tops[i] >= stack.peek().h) {
+				stack.pop();
+			}
+			
+			if(stack.isEmpty()) {
+				ans[i] = 0;
+			} else {
+				ans[i] = stack.peek().idx + 1;
+			}
+			stack.push(new Top(tops[i], i));
+		}
+		
+		for (int i = 0; i < ans.length; i++) {
+			System.out.print(ans[i] + " ");
+		}
+	}//end of main
+}//end of class
