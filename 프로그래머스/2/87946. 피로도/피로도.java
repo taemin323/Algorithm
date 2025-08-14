f7efeab9bf7cf44c82260463c4c6e657576ccbb6
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-08-14 16:53:51 +0900

    [level 2] Title: 피로도, Time: 4.03 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,32 +1,32 @@
 /**
-* 1일 1탐험 가능 던전을 최대한 많이 탐험하는 것이 목적.
-* 현재 피로도 k
-* dungeons[i][0] = 최소 필요 피로도, [i][1] = 소모 피로도
-* 1 <= k <= 5,000
-* 1 <= 던전의 개수 <= 8
-* 1 <= 피로도 <= 1,000
+* 유저의 현재 피로도 k 1 <= k <= 5000
+* 던전의 개수 1 이상 8 이하
+* dungeons[i][0] = 최소 필요 피로도, dungeons[i][1] = 소모 피로도
 */
 import java.util.*;
 
 class Solution {
-    int answer;
+    boolean[] visited;
+    int answer = 0;
     public int solution(int k, int[][] dungeons) {
         
-        dfs(0, k, dungeons);
+        visited = new boolean[dungeons.length];
+        
+        dfs(0,k,dungeons);
         
         return answer;
     }
     
     public void dfs(int cnt, int k, int[][] dungeons) {
+        
         for(int i = 0; i < dungeons.length; i++) {
-            int tmp = dungeons[i][0];
-            if(dungeons[i][0] <= k) {
-                dungeons[i][0] = 9999;
+            if(!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true;
                 dfs(cnt+1, k - dungeons[i][1], dungeons);
-                dungeons[i][0] = tmp;
+                visited[i] = false;
             }
         }
+        
         answer = Math.max(answer, cnt);
     }
-    
 }
