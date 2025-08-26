3de07af64331541cc43e8e62ee433bbee58432df
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-08-26 21:04:58 +0900

    [Gold IV] Title: 전구와 스위치, Time: 180 ms, Memory: 19332 KB -BaekjoonHub

--- 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
+++ 백준/Gold/2138. 전구와 스위치/전구와 스위치.java
@@ -1,41 +1,29 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-/**
- * 2 <= N <= 100,000
- * i번 스위치를 누르면 i-1, i, i+1의 세 개의 전구의 상태가 바뀐다.
- * 불가능한 경우 -1 출력
- */
 public class Main {
 	private static int N;
-	private static int[] lightArr;
-	private static int[] resultArr;
+	private static int[] results;
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
 		N = Integer.parseInt(br.readLine());
-		
-		// lights -> result로 만들어야함.
 		String light = br.readLine();
 		String result = br.readLine();
+		// 입력 완료
 		
-		lightArr = new int[N];
-		resultArr = new int[N];
-		
+		int[] lights = new int[N];
+		results = new int[N];
 		for (int i = 0; i < N; i++) {
-			lightArr[i] = light.charAt(i) - '0';
-			resultArr[i] = result.charAt(i) - '0';
+			lights[i] = light.charAt(i) - '0';
+			results[i] = result.charAt(i) - '0';
 		}
+
+		//case1 첫 번째 전구 스위치 안눌렀을 때
+		int cnt1 = doSwitch(lights.clone(), false);
 		
-		
-		// 첫 번째 스위치 안누름
-		int cnt1 = calculate(lightArr.clone(), false);
-		
-		// 첫 번째 스위치 누름
-		int cnt2 = calculate(lightArr.clone(), true);
+		//case2 첫 번째 전구 스위치 눌렀을 때
+		int cnt2 = doSwitch(lights.clone(), true);
 		
 		int ans = -1;
 		if(cnt1 >= 0 && cnt2 >= 0) ans = Math.min(cnt1, cnt2);
@@ -46,35 +34,35 @@ public class Main {
 		
 	}// end of main
 
-	private static int calculate(int[] arr, boolean pressFirst) {
+	private static int doSwitch(int[] lights, boolean pressFirst) {
 		int cnt = 0;
 		
 		if(pressFirst) {
-			press(arr, 0);
+			toggle(lights, 0);
 			cnt++;
 		}
 		
 		for (int i = 1; i < N; i++) {
-			if(arr[i-1] != resultArr[i-1]) {
-				press(arr, i);
+			if(lights[i-1] != results[i-1]) {
+				toggle(lights, i);
 				cnt++;
 			}
 		}
 		
 		for (int i = 0; i < N; i++) {
-			if(arr[i] != resultArr[i]) return -1;
+			if(lights[i] != results[i]) {
+				return -1;
+			}
 		}
 		
 		return cnt;
 	}
 
-	private static void press(int[] arr, int i) {
-		for (int j = i-1; j <= i+1; j++) {
-			if(j >= 0 && j < N) {
-				arr[j] = arr[j] == 0 ? 1 : 0;
+	private static void toggle(int[] lights, int idx) {
+		for (int i = idx-1; i <= idx+1; i++) {
+			if(i >= 0 && i < N) {
+				lights[i] = lights[i] == 0 ? 1 : 0;
 			}
 		}
 	}
-
-	
 }// end of class
