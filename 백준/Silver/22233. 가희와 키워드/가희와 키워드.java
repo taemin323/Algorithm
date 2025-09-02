2e655fffd90a3425a3afe72dcaea096e28f2cc82
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-09-02 14:13:46 +0900

    [Silver III] Title: 가희와 키워드, Time: 1152 ms, Memory: 218588 KB -BaekjoonHub

--- 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
+++ 백준/Silver/22233. 가희와 키워드/가희와 키워드.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
@@ -18,15 +17,15 @@ public class Main {
 			set.add(br.readLine());
 		}
 		
+		
+		int remain = N;
+		
 		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine(), ",");
-			int len = st.countTokens();
+			String line = br.readLine();
+			String[] words = line.split(",");
 			
-			for (int j = 0; j < len; j++) {
-				String keyWord = st.nextToken();
-				if(set.contains(keyWord)) {
-					set.remove(keyWord);
-				}
+			for (String word : words) {
+				set.remove(word);
 			}
 			
 			sb.append(set.size()).append("\n");
