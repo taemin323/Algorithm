96bdf6fc7162cc7e590cadc68417c535f8c18ebd
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-09-23 15:32:48 +0900

    [Silver IV] Title: 쿠키의 신체 측정, Time: 256 ms, Memory: 23636 KB -BaekjoonHub

--- 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.java
+++ 백준/Silver/20125. 쿠키의 신체 측정/쿠키의 신체 측정.java
@@ -2,18 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-	 
-	static class Point {
-		int r, c;
-		
-		public Point() {
-		}
-		
-		public Point(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-	}
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,47 +14,45 @@ public class Main {
 			for (int j = 0; j < N; j++) {
 				char c = line.charAt(j);
 				
-				if(c == '_') board[i+1][j+1] = 0;
-				else board[i+1][j+1] = 1;
+				if(c == '*') board[i+1][j+1] = 1;
 			}
 		}
 		
-		boolean flag = false;
-		Point heart = new Point(-1,-1);
-		int leftArm = 0;
-		int rightArm = 0;
-		int back = 0;
-		int leftLeg = 0;
-		int rightLeg = 0;
-		for (int i = 1; i < N+1; i++) {
-			for (int j = 1; j < N+1; j++) {
-				if(board[i][j] == 1 && !flag) {
-					flag = true;
-					heart.r = i+1;
-					heart.c = j;
-					sb.append(heart.r).append(" ").append(heart.c).append("\n");
-				} 
-				
-				if(i == heart.r && board[i][j] == 1) {
-					if(j < heart.c) {
-						leftArm++;
-					} else if(j > heart.c) {
-						rightArm++;
-					}
-				}
-				
-				if(i > heart.r && board[i][j] == 1) {
-					if(j == heart.c) {
-						back++;
-					} else if(j == heart.c - 1) {
-						leftLeg++;
-					} else if(j == heart.c + 1) {
-						rightLeg++;
-					}
+		// 심장 찾기
+		int heartR = -1, heartC = -1;
+		outer:
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				if(board[i][j] == 1) {
+					heartR = i + 1;
+					heartC = j;
+					break outer;
 				}
 			}
 		}
-		sb.append(leftArm).append(" ").append(rightArm).append(" ").append(back).append(" ").append(leftLeg).append(" ").append(rightLeg).append(" ");
+		sb.append(heartR).append(" ").append(heartC).append("\n");
+		
+		//팔
+		int leftArm = 0, rightArm = 0;
+		for (int c = heartC-1; c >= 1 && board[heartR][c] == 1; c--) leftArm++;
+		for (int c = heartC+1; c <= N && board[heartR][c] == 1; c++) rightArm++;
+		
+		// 허리
+		int back = 0, backEndR = heartR;
+		for (int r = heartR+1; r <= N && board[r][heartC] == 1; r++) {
+			back++;
+			backEndR = r;
+		}
+		
+		// 다리
+		int leftLeg = 0, rightLeg = 0;
+		for (int r = backEndR + 1; r <= N && heartC - 1 >= 1 && board[r][heartC-1] == 1; r++) leftLeg++;
+		for (int r = backEndR + 1; r <= N && heartC + 1 <= N && board[r][heartC+1] == 1; r++) rightLeg++;
+		
+		
+		
+		sb.append(leftArm).append(" ").append(rightArm).append(" ").append(back)
+		.append(" ").append(leftLeg).append(" ").append(rightLeg);
 		System.out.println(sb.toString());
 	}// end of main
 }// end of class
