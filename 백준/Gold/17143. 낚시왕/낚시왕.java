2583bf9bf9a03a4785b0d0b8dbe6ddf743a4f89f
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-11-18 17:19:09 +0900

    [Gold I] Title: 낚시왕, Time: 552 ms, Memory: 57144 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -1,112 +1,101 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	static class Shark{
-		int r, c, s, d, z;
+    static class Shark {
+        int r, c, s, d, z;
+        Shark(int r, int c, int s, int d, int z) {
+            this.r = r;
+            this.c = c;
+            this.s = s;
+            this.d = d;
+            this.z = z;
+        }
+    }
 
-		public Shark(int r, int c, int s, int d, int z) {
-			this.r = r;
-			this.c = c;
-			this.s = s;
-			this.d = d;
-			this.z = z;
-		}
-	}
-	
-	static int r,c;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int[][] map = new int[r][c];
-		
-		Map<Integer, Shark> sharkList = new HashMap<>();
-		for(int i=0; i<m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int y = Integer.parseInt(st.nextToken())-1;
-			int x = Integer.parseInt(st.nextToken())-1;
-			int s = Integer.parseInt(st.nextToken());
-			int d = Integer.parseInt(st.nextToken());
-			int z = Integer.parseInt(st.nextToken());
-			sharkList.put(i+1, new Shark(y,x,s,d,z));
-			map[y][x] = i+1;
-		}
-		
-		int result=0;
-		int pos =-1;
-		for(int test=0; test<c; test++) {
-			// 1. fishing
-			pos++;
-			for(int i=0; i<r; i++) {
-				if(map[i][pos] >0) {
-					int sharkNumber = map[i][pos];
-					result += sharkList.get(sharkNumber).z; // 상어 잡음
-					// 상어 제거 
-					sharkList.remove(sharkNumber);
-					map[i][pos]=0;
-					break;
-				}
-			}
-			
-			// 2. shark move
-			for(int key : sharkList.keySet()){
-				Shark sh = sharkList.get(key);
-				// d 1위 2아래 3오른쪽 4왼쪽
-				map[sh.r][sh.c] = 0;
-				if(sh.d == 1)  {
-					// r--;
-					if(moving(sh,-1,sh.d)==1) sh.d = 2;
-				}else if(sh.d == 2) { 
-					// r++;
-					if(moving(sh,1,sh.d)==-1) sh.d = 1;
-				}else if(sh.d == 3) {
-					// c++;
-					if(moving(sh,1,sh.d)==-1) sh.d = 4;
-				}else {
-					// c--;
-					if(moving(sh,-1,sh.d)==1) sh.d = 3;
-				}
-			}
-			
-			List<Integer> removeList = new ArrayList<>();
-			// 같은 위치에 있는 상어 서로 경쟁하기
-			for(int key : sharkList.keySet()){
-				Shark sh = sharkList.get(key);
-				if(map[sh.r][sh.c] > 0)	{
-					if(sharkList.get(map[sh.r][sh.c]).z > sh.z) {
-						removeList.add(key);	
-					}else {
-						removeList.add(map[sh.r][sh.c]);
-						map[sh.r][sh.c] =key;
-					}
-				}else {
-					map[sh.r][sh.c] =key;
-				}
-			}
-			for(int key : removeList) {
-				sharkList.remove(key);
-			}
-		}
-		System.out.println(result);
-	}
-	static int moving(Shark sh, int d, int type) {
-		int move = sh.s;
-		while(move>0) {
-			if(type==1|| type==2) {
-				if(sh.r==0) d = 1;
-				else if(sh.r==r-1) d = -1;
-				sh.r+= d;
-			}else {
-				if(sh.c==0) d = 1;
-				else if (sh.c==c-1) d = -1;
-				sh.c+= d;
-			}
-			move--;
-		}
-		return d;
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        Shark[][] grid = new Shark[R + 1][C + 1]; // 1-indexed
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int s = Integer.parseInt(st.nextToken());
+            int d = Integer.parseInt(st.nextToken()); // 1:up, 2:down, 3:right, 4:left
+            int z = Integer.parseInt(st.nextToken());
+            grid[r][c] = new Shark(r, c, s, d, z);
+        }
+
+        long result = 0;
+
+        // For each column where the fisherman moves
+        for (int col = 1; col <= C; col++) {
+            // 1) catch nearest shark in this column (smallest row)
+            for (int row = 1; row <= R; row++) {
+                if (grid[row][col] != null) {
+                    result += grid[row][col].z;
+                    grid[row][col] = null;
+                    break;
+                }
+            }
+
+            // 2) move sharks
+            Shark[][] newGrid = new Shark[R + 1][C + 1];
+
+            for (int r = 1; r <= R; r++) {
+                for (int c = 1; c <= C; c++) {
+                    if (grid[r][c] == null) continue;
+                    Shark sh = grid[r][c];
+
+                    int nr = sh.r;
+                    int nc = sh.c;
+                    int nd = sh.d;
+                    int speed = sh.s;
+
+                    // Optimize speed using cycle
+                    if (nd == 1 || nd == 2) { // vertical
+                        if (R > 1) speed = speed % ((R - 1) * 2);
+                    } else { // horizontal
+                        if (C > 1) speed = speed % ((C - 1) * 2);
+                    }
+
+                    // move step by step (speed <= 198 typically so it's fine)
+                    for (int step = 0; step < speed; step++) {
+                        if (nd == 1) { // up
+                            if (nr == 1) { nd = 2; nr++; }
+                            else nr--;
+                        } else if (nd == 2) { // down
+                            if (nr == R) { nd = 1; nr--; }
+                            else nr++;
+                        } else if (nd == 3) { // right
+                            if (nc == C) { nd = 4; nc--; }
+                            else nc++;
+                        } else if (nd == 4) { // left
+                            if (nc == 1) { nd = 3; nc++; }
+                            else nc--;
+                        }
+                    }
+
+                    // place into newGrid, resolving conflicts by size
+                    if (newGrid[nr][nc] == null) {
+                        newGrid[nr][nc] = new Shark(nr, nc, sh.s, nd, sh.z);
+                    } else {
+                        if (newGrid[nr][nc].z < sh.z) {
+                            newGrid[nr][nc] = new Shark(nr, nc, sh.s, nd, sh.z);
+                        }
+                    }
+                }
+            }
+
+            grid = newGrid; // update grid
+        }
+
+        System.out.println(result);
+    }
 }
