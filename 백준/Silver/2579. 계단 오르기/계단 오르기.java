8adc078fe0f9841c8add9bc2789e32d24679d486
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-11-21 19:30:39 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		int[] step = new int[N+1];
+		for (int i = 1; i <= N; i++) {
+			step[i] = Integer.parseInt(br.readLine());
+		}// 입력 완료
+		
+		if(N == 1) {
+			System.out.println(step[1]);
+			return;
+		}
+		
+		// dp[i] = i번째 계단에 도달할 수 있는 최대 점수
+		long[] dp = new long[N+1];
+		
+		dp[1] = step[1];
+		dp[2] = dp[1] + step[2];
+		
+		for (int i = 3; i <= N; i++) {
+			dp[i] = Math.max(dp[i-2] + step[i], dp[i-3] + step[i-1] + step[i]);
+		}
+		
+		System.out.println(dp[N]);
+		
+	}// end of main
+}// end of class
