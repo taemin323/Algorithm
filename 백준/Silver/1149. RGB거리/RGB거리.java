2c6a16dda92de05e261d49547a54a5177600f7ca
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2025-11-25 15:36:42 +0900

    [Silver I] Title: RGB거리, Time: 112 ms, Memory: 14556 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,8 +1,10 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	
+
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -23,7 +25,6 @@ public class Main {
 			dp[i][0] += Math.min(dp[i-1][1], dp[i-1][2]);
 			dp[i][1] += Math.min(dp[i-1][0], dp[i-1][2]);
 			dp[i][2] += Math.min(dp[i-1][0], dp[i-1][1]);
-			
 		}
 		
 		System.out.println(Math.min(dp[N-1][0], Math.min(dp[N-1][1], dp[N-1][2])));
