e30856e86b83baa31615c8baf0619834b109920d
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-01-08 18:43:59 +0900

    [Gold I] Title: K번째 수, Time: 132 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -2,31 +2,40 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	private static int N;
+	private static int M;
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
-		int N = Integer.parseInt(br.readLine());
-		int k = Integer.parseInt(br.readLine());
-		
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+
 		long left = 1;
-		long right = k;
+		long right = M;
 		long answer = 0;
 		
 		while(left <= right) {
 			long mid = left + (right - left) / 2;
-			long cnt = 0;
 			
-			for(int i = 1; i <= N; i++) {
-				cnt += Math.min(mid/i, N);
-			}
-		
-			if(cnt >= k) {
+			if(calculate(mid) >= M) {
 				answer = mid;
-				right = mid-1;
+				right = mid - 1;
 			} else {
-				left = mid+1;
+				left = mid + 1;
 			}
 		}
 		System.out.println(answer);
 	}// end of main
+
+	private static long calculate(long mid) {
+		long cnt = 0;
+		
+		for (int i = 1; i <= N; i++) {
+			cnt += Math.min(mid/i, N);
+		}
+		return cnt;
+	}
 }// end of class
