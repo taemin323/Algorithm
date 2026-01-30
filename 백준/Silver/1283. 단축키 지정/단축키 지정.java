24ee253b263648e77aa9e614c700a1457798ab96
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-01-30 13:10:22 +0900

    [Silver I] Title: 단축키 지정, Time: 124 ms, Memory: 16452 KB -BaekjoonHub

--- 백준/Silver/1283. 단축키 지정/단축키 지정.java
+++ 백준/Silver/1283. 단축키 지정/단축키 지정.java
@@ -2,57 +2,57 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
 		
 		int N = Integer.parseInt(br.readLine());
-		
-		
 		Set<Character> set = new HashSet<>();
 		
 		for (int i = 0; i < N; i++) {
-			String line = br.readLine();
-			int markIdx = -1; //단축키로 지정할 인덱스
+			String str = br.readLine();
+			int markIdx = -1;
 			
-			
-			//1단계 각 단어의 첫 글자 탐색
-			for (int j = 0; j < line.length(); j++) {
-				if ( j == 0 || line.charAt(j-1) == ' ') {
-					char c = Character.toLowerCase(line.charAt(j));
-					if (!set.contains(c)) {
+			// 1단계
+			for (int j = 0; j < str.length(); j++) {
+				if(j == 0 || str.charAt(j-1) == ' ') {
+					char c = Character.toLowerCase(str.charAt(j));
+					if(!set.contains(c) ) {
 						markIdx = j;
 						break;
 					}
 				}
 			}
 			
-			
-			//2단계 전체 탐색
-			if (markIdx == -1) {
-				for (int j = 0; j < line.length(); j++) {
-					char ch = line.charAt(j);
-					if (ch == ' ') continue;
-					char c = Character.toLowerCase(ch);
-					if(!set.contains(c)) {
+			// 2단계
+			if(markIdx == -1) {
+				for (int j = 0; j < str.length(); j++) {
+					char c = str.charAt(j);
+					if(c == ' ') continue;
+					
+					if(!set.contains(Character.toLowerCase(c))) {
 						markIdx = j;
 						break;
 					}
 				}
 			}
 			
-			//결과 출력 및 단축키 등록
 			if(markIdx != -1) {
-				set.add(Character.toLowerCase(line.charAt(markIdx)));
-				System.out.println(change(line, markIdx));
+				set.add(Character.toLowerCase(str.charAt(markIdx)));
+				System.out.println(change(str, markIdx));
 			} else {
-				System.out.println(line);
+				System.out.println(str);
 			}
 		}
-		
+	
 		
 	}// end of main
 
-	private static String change(String word, int idx) {
-		return word.substring(0, idx) + "[" + word.charAt(idx) + "]" + word.substring(idx+1);
+	private static String change(String str, int idx) {
+		return str.substring(0, idx) + "[" + str.charAt(idx) + "]" + str.substring(idx+1);
 	}
 }// end of class
