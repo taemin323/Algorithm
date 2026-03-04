9bde707b427c1f8e12e698afd13542ab2128670e
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-03-04 17:16:07 +0900

    [level 2] Title: 모음 사전, Time: 3.56 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,17 +1,35 @@
 import java.util.*;
 
 class Solution {
+    private int answer = 0;
+    private int cnt = 0;
+    private String[] base = {"A", "E", "I", "O", "U"};
+    private StringBuilder sb = new StringBuilder();
+    private String target;
+    
     public int solution(String word) {
-
-        int[] weight = {781, 156, 31, 6, 1};
-        String vowels = "AEIOU";
-        int answer = 0;
-
-        for (int i = 0; i < word.length(); i++) {
-            int index = vowels.indexOf(word.charAt(i));
-            answer += index * weight[i] + 1;
-        }
-
+        target = word;
+        
+        dfs(0);
+        
         return answer;
+    }
+    
+    private void dfs(int depth) {
+        if(depth > 5) return;
+        
+        if(depth > 0) {
+            cnt++;
+            if(sb.toString().equals(target)) {
+                answer = cnt;
+                return;
+            }
+        }
+        
+        for(int i = 0; i < base.length; i++) {
+            sb.append(base[i]);
+            dfs(depth + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
     }
 }
