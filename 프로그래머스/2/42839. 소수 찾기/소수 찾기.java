a29c9d6b554d1f16d2381bae85d058c30d6d559e
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-06 17:13:23 +0900

    [level 2] Title: 소수 찾기, Time: 30.34 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,47 +1,33 @@
 import java.util.*;
 
 class Solution {
-    static Set<Integer> set;
-    static boolean[] visited;
-    
     public int solution(String numbers) {
         int answer = 0;
-        set = new HashSet<>();
-        visited = new boolean[numbers.length()];
-        dfs(numbers, "", 0);
         
-        for (Integer num: set) {
+        HashSet<Integer> set = new HashSet<>();
+        
+        perm("", numbers, set);        
+        
+        for(int num : set) {
             if(isPrime(num)) answer++;
         }
-    
+        
         return answer;
     }
     
-    public static void dfs(String numbers, String curr, int depth) {
-        //numbers의 끝까지 다 탐색했다면 종료
-        if (depth > numbers.length()) {
-            return;
-        }
+    public void perm(String prev, String numbers, HashSet<Integer> set) {
+        if(!prev.equals("")) set.add(Integer.parseInt(prev));
         
-        for (int i = 0; i < numbers.length(); i++) {
-            if(!visited[i]) {
-                visited[i] = true;
-                set.add(Integer.parseInt(curr + numbers.charAt(i)));
-                dfs(numbers, curr + numbers.charAt(i), depth + 1);
-                visited[i] = false;
-            }
+        for(int i = 0; i < numbers.length(); i++) {
+            perm(prev + numbers.charAt(i), numbers.substring(0,i) + numbers.substring(i+1, numbers.length()), set);
         }
     }
     
-    public static boolean isPrime(int n) {
-        if (n < 2) {
-            return false;
-        }
+    public boolean isPrime(int n) {
+        if(n == 0 || n == 1) return false;
         
-        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
-            if(n % i == 0) {
-                return false;
-            }
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) return false;
         }
         
         return true;
