77b29e9b6c98560ba9ea22540197a41bfdcf9a30
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-06 15:33:49 +0900

    [level 1] Title: 모의고사, Time: 0.84 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,32 +2,44 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] supo1 = {1, 2, 3, 4, 5};
-        int[] supo2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] supo3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] scores = {0,0,0};
+        int cntA = 0;
+        int cntB = 0;
+        int cntC = 0;
         
-        for (int i = 0; i < answers.length; i++) {
-            if(answers[i] == supo1[i % 5]) scores[0]++;
-            if(answers[i] == supo2[i % 8]) scores[1]++;
-            if(answers[i] == supo3[i % 10]) scores[2]++;
+        int n = answers.length;
+        
+        int[] b = {2,1,2,3,2,4,2,5};
+        int[] c = {3,3,1,1,2,2,4,4,5,5};
+        
+        // a 계산
+        for(int i = 0; i < n; i++) {
+            if(answers[i] == (i%5) + 1) cntA++;
+            
+            if(answers[i] == b[(i % 8)]) cntB++;
+            
+            if(answers[i] == c[(i % 10 )]) cntC++;
         }
         
-        int max = Math.max(scores[0], Math.max(scores[1], scores[2]));
+        int[] arr = new int[4];
+        arr[1] = cntA;
+        arr[2] = cntB;
+        arr[3] = cntC;
+
+        int max = Math.max(cntA, Math.max(cntB, cntC));
+        
         
         List<Integer> list = new ArrayList<>();
-        
-        for (int i = 0; i < scores.length; i++) {
-            if(max == scores[i]) list.add(i+1);
+        for(int i = 1; i <= 3; i++) {
+            if(arr[i] == max) {
+                list.add(i);
+            }
         }
         
-        int[] result = new int[list.size()];
-        
-        for(int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
         }
         
-        return result;
-    
+        return answer;
     }
 }
