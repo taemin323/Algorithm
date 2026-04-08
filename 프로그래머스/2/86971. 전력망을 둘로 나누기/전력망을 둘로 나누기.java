339a2ac8af170fd92e3eada2f01c6bd8c8e2fcf5
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-08 16:49:42 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.47 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,78 +1,42 @@
 import java.util.*;
 
 class Solution {
-    
-    List<List<Integer>> tree;
-    
+    int[][] tree;
+    boolean[] visited;
+    int N;
+    int answer = Integer.MAX_VALUE;
+     
     public int solution(int n, int[][] wires) {
-        int answer = Integer.MAX_VALUE;
+        N = n;
+        tree = new int[n+1][n+1];
+        visited = new boolean[n+1];
         
-        // 트리 생성
-        tree = new ArrayList<>();
-        for(int i= 0; i <= n; i++) {
-            tree.add(new ArrayList<>());
-        }
         
-        // 트리 구성
+        //tree 구성
         for(int i = 0; i < wires.length; i++) {
             int from = wires[i][0];
             int to = wires[i][1];
             
-            // 양방향
-            tree.get(from).add(to);
-            tree.get(to).add(from);
+            tree[from][to] = 1;
+            tree[to][from] = 1;
         }
         
-        // 완전 탐색
-        for(int i = 0; i < wires.length; i++) {
-            int curFrom = wires[i][0];
-            int curTo = wires[i][1];
-            
-            // 간선 끊기
-            tree.get(curFrom).remove(Integer.valueOf(curTo));
-            tree.get(curTo).remove(Integer.valueOf(curFrom));
-            
-            boolean[] visited = new boolean[n+1];
-            int cnt = 0;
-            for(int j = 1; j <= n; j++) {
-                if(!visited[j]) {
-                    bfs(j, visited);
-                    break;
-                }
-                
-            }
-            
-            //차이 계산
-            int group1 = 0;
-            for(int k = 1; k <= n; k++) {
-                if(visited[k]) group1++;
-            }
-            int group2 = n - group1;
-            
-            answer = Math.min(answer, Math.abs(group1-group2));
-            
-            //원복
-            Arrays.fill(visited, false);
-            tree.get(curFrom).add(curTo);
-            tree.get(curTo).add(curFrom);
-        }
+        dfs(1);
         return answer;
     }
     
-    void bfs(int idx, boolean[] visited) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(idx);
+    public int dfs(int cur) {
+        visited[cur] = true;
+        int child = 1;
         
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-            
-            visited[cur] = true;
-            
-            for(int i : tree.get(cur)) {
-                if(!visited[i]) {
-                    q.add(i);
-                }
+        for(int i = 1; i <= N; i++) {
+            if(!visited[i] && tree[cur][i] == 1) {
+                visited[i] = true;
+                child += dfs(i);
             }
         }
+        
+        answer = Math.min(answer, Math.abs(child - (N-child)));
+        return child;
     }
 }
