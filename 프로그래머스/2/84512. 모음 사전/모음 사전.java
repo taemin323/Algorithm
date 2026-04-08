60b8a5ea2b96b1628b37f1ae921c2784c0ef2225
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-08 18:46:00 +0900

    [level 2] Title: 모음 사전, Time: 28.79 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,35 +1,23 @@
 import java.util.*;
 
 class Solution {
-    private int answer = 0;
-    private int cnt = 0;
-    private String[] base = {"A", "E", "I", "O", "U"};
-    private StringBuilder sb = new StringBuilder();
-    private String target;
+    List<String> list = new ArrayList<>();
     
     public int solution(String word) {
-        target = word;
         
-        dfs(0);
+        dfs("", 0);
         
-        return answer;
+        return list.indexOf(word);
     }
     
-    private void dfs(int depth) {
-        if(depth > 5) return;
+    void dfs(String str, int len) {
+        if(len > 5) return;
         
-        if(depth > 0) {
-            cnt++;
-            if(sb.toString().equals(target)) {
-                answer = cnt;
-                return;
-            }
-        }
+        list.add(str);
         
-        for(int i = 0; i < base.length; i++) {
-            sb.append(base[i]);
-            dfs(depth + 1);
-            sb.deleteCharAt(sb.length() - 1);
+        for(int i = 0; i < 5; i++) {
+            String next = str + "AEIOU".charAt(i);
+            dfs(next, len+1);
         }
     }
 }
