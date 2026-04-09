6ca2663a60eb1834b3e200926e76089d49c0a89d
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-09 11:40:00 +0900

    [level 1] Title: 모의고사, Time: 3.29 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,29 +1,25 @@
 import java.util.*;
-import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] answers) {
         int[] a = {1,2,3,4,5};
         int[] b = {2,1,2,3,2,4,2,5};
         int[] c = {3,3,1,1,2,2,4,4,5,5};
-        int[] scores = new int[4];
+        int[] scores = new int[3];
         
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == a[i % 5]) scores[1]++;
-            if(answers[i] == b[i % 8]) scores[2]++;
-            if(answers[i] == c[i % 10]) scores[3]++;
+            if(answers[i] == a[i % 5]) scores[0]++;
+            if(answers[i] == b[i % 8]) scores[1]++;
+            if(answers[i] == c[i % 10]) scores[2]++;
         }
-        
-        int maxCnt = Math.max(scores[1], Math.max(scores[2], scores[3]));
+        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
         
         List<Integer> list = new ArrayList<>();
-        for(int i = 1; i <=3; i++) {
-            if(maxCnt == scores[i]) list.add(i);
+        
+        for(int i = 0; i < 3; i++) {
+            if(scores[i] == maxScore) list.add(i+1);
         }
         
-        // return list.stream().mapToInt(i -> i).toArray();
-        return IntStream.range(1,4)
-                        .filter(i -> scores[i] == maxCnt)
-                        .toArray();
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
