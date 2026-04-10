8cc1154166820bf1833a35b85cf89d7df816039e
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-10 14:53:16 +0900

    [level 2] Title: 피로도, Time: 10.59 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,26 +1,47 @@
 import java.util.*;
 
 class Solution {
+    List<int[]> list = new ArrayList<>();
+    int[] arr;
+    boolean[] visited;
     int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-        dfs(0, dungeons, k);
+        arr = new int[dungeons.length];
+        visited = new boolean[dungeons.length];
+        
+        perm(0, dungeons, k);
+        
         return answer;
     }
     
-    public void dfs(int cnt, int[][] dungeons, int k) {
-        answer = Math.max(answer, cnt);
+    void perm(int cnt, int[][] dungeons, int k) {
+        if(cnt == dungeons.length) {
+            calculate(arr, dungeons, k);
+        }
         
         for(int i = 0; i < dungeons.length; i++) {
-            int a = dungeons[i][0];
-            
-            if(k >= dungeons[i][0]) {
-                dungeons[i][0] = 5001;
-                
-                dfs(cnt+1, dungeons, (k - dungeons[i][1]));
-                
-                dungeons[i][0] = a;
+            if(!visited[i]) {
+                visited[i] = true;
+                arr[cnt] = i;
+                perm(cnt+1, dungeons, k);
+                visited[i] = false;
             }
         }
+    }
+    
+    void calculate(int[] arr, int[][]dungeons, int k) {
+        int cnt = 0;
+        
+        for(int i = 0; i < arr.length; i++) {
+            int idx = arr[i];
+            
+            if(k >= dungeons[idx][0]) {
+                k -= dungeons[idx][1];
+                cnt++;
+            }
+        }
+        
+        answer = Math.max(cnt, answer);
     }
 }
