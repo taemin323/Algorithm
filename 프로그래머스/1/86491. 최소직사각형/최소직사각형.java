557dbbcc5e77b4ab8f8433de79a46dd12166903b
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-13 22:08:18 +0900

    [level 1] Title: 최소직사각형, Time: 1.66 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] sizes) {
-        int w = 0;
         int h = 0;
+        int w = 0;
         
         for(int i = 0; i < sizes.length; i++) {
-            w = Math.max(w, Math.max(sizes[i][0], sizes[i][1]));
-            h = Math.max(h, Math.min(sizes[i][0], sizes[i][1]));
+            h = Math.max(h, Math.max(sizes[i][0], sizes[i][1]));
+            w = Math.max(w, Math.min(sizes[i][0], sizes[i][1]));
         }
         
-        
-        int answer = w * h;
+        int answer = h * w;
         return answer;
     }
 }
