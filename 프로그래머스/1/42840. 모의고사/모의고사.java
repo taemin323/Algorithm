978c336690413e33b4c3267b65a2af9694b05f55
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-13 21:54:50 +0900

    [level 1] Title: 모의고사, Time: 3.48 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -5,21 +5,22 @@ class Solution {
         int[] a = {1,2,3,4,5};
         int[] b = {2,1,2,3,2,4,2,5};
         int[] c = {3,3,1,1,2,2,4,4,5,5};
-        int[] scores = new int[3];
+        int[] scores = new int[4];
         
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == a[i % 5]) scores[0]++;
-            if(answers[i] == b[i % 8]) scores[1]++;
-            if(answers[i] == c[i % 10]) scores[2]++;
+            if(answers[i] == a[i % 5]) scores[1]++;
+            if(answers[i] == b[i % 8]) scores[2]++;
+            if(answers[i] == c[i % 10]) scores[3]++;
         }
-        int maxScore = Math.max(scores[0], Math.max(scores[1], scores[2]));
+        
+        int maxScore = Math.max(scores[1], Math.max(scores[2], scores[3]));
         
         List<Integer> list = new ArrayList<>();
         
-        for(int i = 0; i < 3; i++) {
-            if(scores[i] == maxScore) list.add(i+1);
+        for(int i = 0; i < 4; i++) {
+            if(maxScore == scores[i]) list.add(i);
         }
-        
+         
         return list.stream().mapToInt(i -> i).toArray();
     }
 }
