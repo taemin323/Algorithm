07b42fb176e698760e9f7f48f9ae496026db7cd0
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-14 13:07:52 +0900

    [level 2] Title: 피로도, Time: 4.99 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,39 +1,25 @@
 import java.util.*;
 
 class Solution {
-    boolean[] visited;
-    int answer = Integer.MIN_VALUE;
+    int answer = 0;
     
     public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-        dfs(0, k, dungeons);
-        
+        dfs(0,k,dungeons);
         return answer;
     }
     
-    void dfs(int depth, int k, int[][] dungeons) {
-        if(check(dungeons, k)) {
-            int cnt = 0;
-            for(boolean b : visited) {
-                if(b) cnt++;
-            }
-            
-            answer = Math.max(answer, cnt);
-        }
+    void dfs(int cnt, int k, int[][] dungeons) {
+        answer = Math.max(answer, cnt);
         
         for(int i = 0; i < dungeons.length; i++) {
-            if(k >= dungeons[i][0] && !visited[i]) {
-                visited[i] = true;
-                dfs(depth+1, k - dungeons[i][1], dungeons);
-                visited[i] = false;
+            if(k >= dungeons[i][0]) {
+                int cur = dungeons[i][0];
+                dungeons[i][0] = 50001;
+                
+                dfs(cnt+1, k-dungeons[i][1], dungeons);
+                
+                dungeons[i][0] = cur;
             }
         }
-    }
-    
-    boolean check(int[][] dungeons, int k) {
-        for(int i = 0; i < visited.length; i++) {
-            if(!visited[i] && k >= dungeons[i][0]) return false;
-        }
-        return true;
     }
 }
