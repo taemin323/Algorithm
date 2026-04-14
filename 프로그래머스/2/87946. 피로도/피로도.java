866ced58ebebabdfaedfb97941f14826a9532fcf
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-14 13:07:23 +0900

    [level 2] Title: 피로도, Time: 4.99 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,47 +1,39 @@
 import java.util.*;
 
 class Solution {
-    List<int[]> list = new ArrayList<>();
-    int[] arr;
     boolean[] visited;
-    int answer = 0;
+    int answer = Integer.MIN_VALUE;
     
     public int solution(int k, int[][] dungeons) {
-        arr = new int[dungeons.length];
         visited = new boolean[dungeons.length];
-        
-        perm(0, dungeons, k);
+        dfs(0, k, dungeons);
         
         return answer;
     }
     
-    void perm(int cnt, int[][] dungeons, int k) {
-        if(cnt == dungeons.length) {
-            calculate(arr, dungeons, k);
+    void dfs(int depth, int k, int[][] dungeons) {
+        if(check(dungeons, k)) {
+            int cnt = 0;
+            for(boolean b : visited) {
+                if(b) cnt++;
+            }
+            
+            answer = Math.max(answer, cnt);
         }
         
         for(int i = 0; i < dungeons.length; i++) {
-            if(!visited[i]) {
+            if(k >= dungeons[i][0] && !visited[i]) {
                 visited[i] = true;
-                arr[cnt] = i;
-                perm(cnt+1, dungeons, k);
+                dfs(depth+1, k - dungeons[i][1], dungeons);
                 visited[i] = false;
             }
         }
     }
     
-    void calculate(int[] arr, int[][]dungeons, int k) {
-        int cnt = 0;
-        
-        for(int i = 0; i < arr.length; i++) {
-            int idx = arr[i];
-            
-            if(k >= dungeons[idx][0]) {
-                k -= dungeons[idx][1];
-                cnt++;
-            }
+    boolean check(int[][] dungeons, int k) {
+        for(int i = 0; i < visited.length; i++) {
+            if(!visited[i] && k >= dungeons[i][0]) return false;
         }
-        
-        answer = Math.max(cnt, answer);
+        return true;
     }
 }
