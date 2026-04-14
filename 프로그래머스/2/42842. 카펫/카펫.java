f9af7ad4aed3f78d5e371a1648c0fa4ebe96072b
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-14 13:39:37 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 93.6 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,12 +3,18 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        for(int w = 0; w < 5000; w++) {
-            for(int h = 0; h < 5000; h++) {
-                if(w*h == brown + yellow && (w-2)*(h-2) == yellow) {
-                    answer[0] = w;
-                    answer[1] = h;
-                }
+        
+        int carpet = brown + yellow;
+        
+        for(int i = 3; i <= Math.sqrt(carpet); i++) {
+            int h = i;
+            int w = carpet / h;
+            
+            if((w-2)*(h-2) == yellow) {
+                answer[0] = w;
+                answer[1] = h;
+                break;
+            
             }
         }
         
