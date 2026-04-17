2bd034378baf15e0cd7db6d81de8bef9ccdb3796
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-17 17:44:04 +0900

    [level 3] Title: 네트워크, Time: 0.62 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 class Solution {
     int[] parent;
-    
     public int solution(int n, int[][] computers) {
         parent = new int[n];
         for(int i = 0; i < n; i++) {
             parent[i] = i;
-        }
+        }       
+        
+        int answer = 0;
         
         for(int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++) {
-                if(computers[i][j] == 1) {
-                    union(i, j);
-                }
+            for(int j = 0; j < n; j++) {
+                if(computers[i][j] == 1) union(i, j);
             }
         }
         
@@ -21,7 +20,6 @@ class Solution {
         for(int i : parent) {
             set.add(find(i));
         }
-        
         return set.size();
     }
     
@@ -29,13 +27,12 @@ class Solution {
         int pi = find(i);
         int pj = find(j);
         
-        if(pi != pj) {
-            parent[pj] = pi;
-        }
+        if(pi != pj) parent[pj] = pi;
     }
     
-    int find(int i) {
-        if(parent[i] == i) return i;
-        return parent[i] = find(parent[i]);
+    int find(int x) {
+        if(parent[x] == x) return x;
+        
+        return parent[x] = find(parent[x]);
     }
 }
