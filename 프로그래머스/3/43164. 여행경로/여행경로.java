ee2326503433a0290b8efca55eff0db7abb2a70d
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-22 22:40:37 +0900

    [level 3] Title: 여행경로, Time: 0.46 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,29 +1,29 @@
 import java.util.*;
 
 class Solution {
-    private boolean[] visited;
-    private String[] answer;
-    private boolean flag;
+    boolean[] visited;
+    String[] answer;
+    boolean flag;
     
     public String[] solution(String[][] tickets) {
-        //정렬
-        Arrays.sort(tickets, (a,b) ->{
+        Arrays.sort(tickets, (a,b) -> {
             return a[1].compareTo(b[1]);
         });
         
         visited = new boolean[tickets.length];
-        String[] path = new String[tickets.length + 1];
+        String[] path = new String[tickets.length+1];
         path[0] = "ICN";
         
         dfs("ICN", tickets, path, 0);
         
+        
         return answer;
     }
     
-    private void dfs(String cur, String[][] tickets, String[] path, int cnt) {
+    void dfs(String cur, String[][] tickets, String[] path, int depth) {
         if(flag) return;
         
-        if(cnt == tickets.length) {
+        if(depth == tickets.length) {
             answer = path.clone();
             flag = true;
             return;
@@ -32,9 +32,9 @@ class Solution {
         for(int i = 0; i < tickets.length; i++) {
             if(!visited[i] && tickets[i][0].equals(cur)) {
                 visited[i] = true;
-                path[cnt+1] = tickets[i][1];
+                path[depth+1] = tickets[i][1];
                 
-                dfs(tickets[i][1], tickets, path, cnt+1);
+                dfs(tickets[i][1], tickets, path, depth+1);
                 visited[i] = false;
             }
         }
