595e6f3f6f8f9cfa1ca352914a5a1322d69787b7
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-22 17:59:26 +0900

    [level 3] Title: 아이템 줍기, Time: 1.28 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,70 +1,78 @@
 import java.util.*;
 
 class Solution {
-    private int[][] map;
-    private boolean[][] visited;
-    private int[] dr = {-1,1,0,0};
-    private int[] dc = {0,0,-1,1};
-    
-    
+    int[][] map;
+    boolean[][] visited;
+    int[] dr = {-1,1,0,0};
+    int[] dc = {0,0,-1,1};
+    int answer = 0;
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        map = new int[102][102];
-        visited = new boolean[102][102];
         
+        // 맵 확장 생성
+        map = new int[101][101];
+        visited = new boolean[101][101];
+        
+        // 경로 생성
         for(int i = 0; i < rectangle.length; i++) {
-            for(int j = 0; j < 4; j++) {
-                int x1 = rectangle[i][0]*2;
-                int y1 = rectangle[i][1]*2;
-                int x2 = rectangle[i][2]*2;
-                int y2 = rectangle[i][3]*2;
-                
-                makeMap(x1, y1, x2, y2);
-            }
+            int leftC = rectangle[i][0];
+            int leftR = rectangle[i][1];
+            int rightC = rectangle[i][2];
+            int rightR = rectangle[i][3];
+            
+            makeRoute(leftC*2, leftR*2, rightC*2, rightR*2);
         }
         
-        return bfs(characterX*2, characterY*2, itemX*2, itemY*2, 0);
+        bfs(characterX*2, characterY*2, itemX*2, itemY*2);
+        return answer/2;
     }
     
-    private void makeMap(int x1, int y1, int x2, int y2) {
-        for(int i = x1; i <= x2; i++) {
-            for(int j = y1; j <= y2; j++) {
-                if(map[j][i] == -1) continue;
+    void makeRoute(int leftC, int leftR, int rightC, int rightR) {
+        // 테두리를 포함한 모든 영역 1로 채우기
+        // 그 중 내부는 -1로 덮기
+        
+        for(int r = leftR; r <= rightR; r++) {
+            for(int c = leftC; c <= rightC; c++) {
                 
-                if(i == x1 || i == x2 || j == y1 || j == y2) {
-                    map[j][i] = 1;
-                } else {
-                    map[j][i] = -1;
+                // 사각형 내부는 -1 처리
+                if(r > leftR && r < rightR && c > leftC && c < rightC) {
+                    map[r][c] = -1;
+                } else if(map[r][c] != -1) {// 다른 사각형 내부가 아니라면
+                    // 테두리를 경로에 포함
+                    map[r][c] = 1;
                 }
             }
         }
+        
+        
     }
     
-    private int bfs(int startC, int startR, int endC, int endR, int dist) {
+    void bfs(int startC, int startR, int targetC, int targetR) {
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{startR, startC, dist});
+        q.offer(new int[]{startR, startC});
         visited[startR][startC] = true;
         
         while(!q.isEmpty()) {
-            int[] cur = q.poll();
+            int qSize = q.size();
+            for(int k = 0; k < qSize; k++) {
+                int[] cur = q.poll();
+                int curR = cur[0];
+                int curC = cur[1];
                 
-            int curR = cur[0];
-            int curC = cur[1];
-            int curDist = cur[2];
+                if(curR == targetR && curC == targetC) return;
                 
-            if(curR == endR && curC == endC) return curDist/2;
-                
-            for(int d = 0; d < 4; d++) {
-                int nr = curR + dr[d];
-                int nc = curC + dc[d];
+                for(int d = 0; d < 4; d++) {
+                    int nr = curR + dr[d];
+                    int nc = curC + dc[d];
                     
-                if(nr < 0 || nr >= 102 || nc < 0 || nc >= 102) continue;
+                    if(nr < 0 || nr >= 101 || nc < 0 || nc >= 101) continue;
                     
-                if(!visited[nr][nc] && map[nr][nc] == 1) {
-                    visited[nr][nc] = true;
-                    q.offer(new int[]{nr, nc, curDist+1});
+                    if(!visited[nr][nc] && map[nr][nc] == 1) {
+                        visited[nr][nc] = true;
+                        q.offer(new int[]{nr, nc});
+                    }
                 }
             }
+            answer++;
         }
-        return dist/2;
     }
 }
