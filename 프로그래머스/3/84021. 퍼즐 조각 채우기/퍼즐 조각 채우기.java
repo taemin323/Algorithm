92e9252b8ddc7a918d19479458ff7cdeda906151
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-28 03:16:49 +0900

    [level 3] Title: 퍼즐 조각 채우기, Time: 9.79 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
+++ 프로그래머스/3/84021. 퍼즐 조각 채우기/퍼즐 조각 채우기.java
@@ -1,48 +1,43 @@
 import java.util.*;
 
 class Solution {
-    
     int[] dr = {-1,1,0,0};
     int[] dc = {0,0,-1,1};
+    List<List<int[]>> spaces = new ArrayList<>();
+    List<List<int[]>> blocks = new ArrayList<>();
     
     public int solution(int[][] game_board, int[][] table) {
+        int n = game_board.length;
+        int m = game_board[0].length;
+        boolean[][] visited = new boolean[n][m];
         
-        List<List<int[]>> emptySpace = new ArrayList<>();
-        List<List<int[]>> blocks = new ArrayList<>();
-        
-        int N = game_board.length;
-        int M = game_board[0].length;
-        boolean[][] visited = new boolean[N][M];
-        
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 if(!visited[i][j] && game_board[i][j] == 0) {
-                    emptySpace.add(bfs(i,j,N,M,visited,game_board,0));
+                    spaces.add(bfs(i,j,n,m,game_board,visited,0));
                 }
             }
-        }
+        } 
         
-        N = table.length;
-        M = table[0].length;
-        visited = new boolean[N][M];
+        n = table.length;
+        m = table[0].length;
+        visited = new boolean[n][m];
         
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < M; j++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 if(!visited[i][j] && table[i][j] == 1) {
-                    blocks.add(bfs(i,j,N,M,visited,table,1));
+                    blocks.add(bfs(i,j,n,m,table,visited,1));
                 }
             }
         }
-        
-        return match(emptySpace, blocks);
+        return match(spaces, blocks);
     }
     
-    List<int[]> bfs(int r, int c, int N, int M, boolean[][] visited, int[][] board, int target) {
+    List<int[]> bfs(int i, int j, int n, int m,int[][] board, boolean[][] visited, int target) {
         List<int[]> result = new ArrayList<>();
-        
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[] {r, c});
-        visited[r][c] = true;
+        q.offer(new int[] {i, j});
+        visited[i][j] = true;
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
@@ -54,63 +49,58 @@ class Solution {
                 int nr = curR + dr[d];
                 int nc = curC + dc[d];
                 
-                if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-                
+                if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
                 
                 if(!visited[nr][nc] && board[nr][nc] == target) {
                     visited[nr][nc] = true;
-                    q.add(new int[] {nr, nc});
+                    q.offer(new int[] {nr, nc});
                 }
             }
         }
-        
         return calculate(result);
     }
     
-    // 정규화 메서드
-    List<int[]> calculate(List<int[]> list) {
+    List<int[]> calculate(List<int[]> result) {
         int minR = Integer.MAX_VALUE;
         int minC = Integer.MAX_VALUE;
         
-        for(int[] l : list) {
-            minR = Math.min(minR, l[0]);
-            minC = Math.min(minC, l[1]);
+        for(int[] i : result) {
+            minR = Math.min(minR, i[0]);
+            minC = Math.min(minC, i[1]);
         }
         
-        for(int[] l : list) {
-            l[0] -= minR;
-            l[1] -= minC;
-        }
-        
-        return list;
-    }
-    
-    int match(List<List<int[]>> emptySpace, List<List<int[]>> blocks) {
-        int result = 0;
-        boolean[] visited = new boolean[blocks.size()];
-        
-        for(int i = 0; i < emptySpace.size(); i++) {
-            List<int[]> space = emptySpace.get(i);
-            
-            for(int j = 0; j < blocks.size(); j++) {
-                if(visited[j]) continue;
-                
-                List<int[]> block = blocks.get(j);
-                if(rotateAndCompare(space, block)) {
-                    result += block.size();
-                    visited[j] = true;
-                    break;
-                }
-            }
+        for(int[] i : result) {
+            i[0] -= minR;
+            i[1] -= minC;
         }
         
         return result;
     }
     
-    boolean rotateAndCompare(List<int[]> space, List<int[]> block) {
+    int match(List<List<int[]>> spaces, List<List<int[]>> blocks) {
+        int result = 0;
+        boolean[] used = new boolean[blocks.size()];
+        
+        for(int i = 0; i < spaces.size(); i++) {
+            List<int[]> space = spaces.get(i);
+            for(int j = 0; j < blocks.size(); j++) {
+                if(used[j]) continue;
+                
+                List<int[]> block = blocks.get(j);
+                
+                if(rotatedAndCompare(space, block)) {
+                    used[j] = true;
+                    result += block.size();
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+    
+    boolean rotatedAndCompare(List<int[]> space, List<int[]> block) {
         if(space.size() != block.size()) return false;
         
-        // 회전시켰을 때 4개의 모양을 space와 비교
         List<int[]> rotate = block;
         for(int i = 0; i < 4; i++) {
             if(compare(space, rotate)) return true;
@@ -133,7 +123,6 @@ class Solution {
             
             if(s[0] != b[0] || s[1] != b[1]) return false;
         }
-        
         return true;
     }
     
@@ -143,15 +132,14 @@ class Solution {
         int minR = Integer.MAX_VALUE;
         int minC = Integer.MAX_VALUE;
         
-        //시계방향으로 90도 회전
-        for(int[] b : block) {
-            int r = b[1];
-            int c = -b[0];
+        for(int[] i : block) {
+            int r = i[1];
+            int c = -i[0];
             
             result.add(new int[] {r,c});
             
-            if(r < minR) minR = r;
-            if(c < minC) minC = c;
+            minR = Math.min(minR, r);
+            minC = Math.min(minC, c);
         }
         
         //정규화
@@ -159,7 +147,6 @@ class Solution {
             i[0] -= minR;
             i[1] -= minC;
         }
-        
         return result;
     }
 }
