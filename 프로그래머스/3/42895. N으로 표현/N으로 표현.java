4a2551125063b0cbc5dbb2100a8fefdf0440f6b8
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-27 14:22:22 +0900

    [level 3] Title: N으로 표현, Time: 49.97 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,45 +1,33 @@
 import java.util.*;
-/**
-* 1 <= N <= 9
-* 1 <= number <= 32,000
-*/
 
 class Solution {
     public int solution(int N, int number) {
         
+        // dp[i] : N을 i개 사용해서 만들 수 있는 수들의 집합
         List<Set<Integer>> dp = new ArrayList<>();
         for(int i = 0; i <= 8; i++) {
             dp.add(new HashSet<>());
         }
         
-        // N 연속인 숫자(N, NN, NNN...)
-        int tmp = 0;
-        for(int i = 1; i <=8; i++) {
-            tmp = tmp * 10 + N;
-            dp.get(i).add(tmp);
+        String str = "";
+        for(int i = 1; i <= 8; i++) {
+            str += N;
+            dp.get(i).add(Integer.parseInt(str));
         }
         
-        // 조합으로 나머지 연산 구하기
         for(int i = 1; i <= 8; i++) {
-            //j개의 N과 (i-j)개의 N을 조합
             for(int j = 1; j < i; j++) {
-                for(int a: dp.get(j)) {
-                    for(int b: dp.get(i - j)) {
-                        dp.get(i).add(a + b);
-                        dp.get(i).add(a - b);
-                        dp.get(i).add(a * b);
-                        if (b != 0) {
-                            dp.get(i).add(a / b);
-                        }
+                for(int a : dp.get(j)) {
+                    for(int b : dp.get(i-j)) {
+                        dp.get(i).add(a+b);
+                        dp.get(i).add(a-b);
+                        dp.get(i).add(a*b);
+                        if(b != 0) dp.get(i).add(a/b);
                     }
                 }
             }
-            if(dp.get(i).contains(number)) {
-                return i;
-            }
+            if(dp.get(i).contains(number)) return i;
         }
-        
-     
         
         return -1;
     }
