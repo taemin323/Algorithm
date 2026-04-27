aa3ab11c2a573a272248f9bbd8d559ecf61aca99
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-27 13:14:48 +0900

    [level 3] Title: 아이템 줍기, Time: 0.91 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -2,77 +2,69 @@ import java.util.*;
 
 class Solution {
     int[][] map;
-    boolean[][] visited;
     int[] dr = {-1,1,0,0};
     int[] dc = {0,0,-1,1};
-    int answer = 0;
+    
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+        int answer = 0;
+        boolean[][] visited = new boolean[102][102];
         
-        // 맵 확장 생성
-        map = new int[101][101];
-        visited = new boolean[101][101];
-        
-        // 경로 생성
+        map = new int[102][102];
         for(int i = 0; i < rectangle.length; i++) {
-            int leftC = rectangle[i][0];
-            int leftR = rectangle[i][1];
-            int rightC = rectangle[i][2];
-            int rightR = rectangle[i][3];
+            int lc = rectangle[i][0]*2;
+            int lr = rectangle[i][1]*2;
+            int rc = rectangle[i][2]*2;
+            int rr = rectangle[i][3]*2;
             
-            makeRoute(leftC*2, leftR*2, rightC*2, rightR*2);
+            makeRoute(lc, lr, rc , rr);
         }
         
-        bfs(characterX*2, characterY*2, itemX*2, itemY*2);
-        return answer/2;
-    }
-    
-    void makeRoute(int leftC, int leftR, int rightC, int rightR) {
-        // 테두리를 포함한 모든 영역 1로 채우기
-        // 그 중 내부는 -1로 덮기
-        
-        for(int r = leftR; r <= rightR; r++) {
-            for(int c = leftC; c <= rightC; c++) {
-                
-                // 사각형 내부는 -1 처리
-                if(r > leftR && r < rightR && c > leftC && c < rightC) {
-                    map[r][c] = -1;
-                } else if(map[r][c] != -1) {// 다른 사각형 내부가 아니라면
-                    // 테두리를 경로에 포함
-                    map[r][c] = 1;
-                }
-            }
-        }
-        
-        
-    }
-    
-    void bfs(int startC, int startR, int targetC, int targetR) {
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{startR, startC});
-        visited[startR][startC] = true;
+        q.offer(new int[] {characterY*2, characterX*2, 0});
+        visited[characterY*2][characterX*2] = true;
         
         while(!q.isEmpty()) {
-            int qSize = q.size();
-            for(int k = 0; k < qSize; k++) {
-                int[] cur = q.poll();
-                int curR = cur[0];
-                int curC = cur[1];
+            int[] cur = q.poll();
+            int curR = cur[0];
+            int curC = cur[1];
+            int curDist = cur[2];
+            
+            if(curR == itemY*2 && curC == itemX*2) {
+                answer = curDist;
+                return answer/2;
+            }
+            
+            for(int d = 0; d < 4; d++) {
+                int nr = curR + dr[d];
+                int nc = curC + dc[d];
                 
-                if(curR == targetR && curC == targetC) return;
+                if(nr < 0 || nr >= 102 || nc < 0 || nc >= 102) continue;
                 
-                for(int d = 0; d < 4; d++) {
-                    int nr = curR + dr[d];
-                    int nc = curC + dc[d];
-                    
-                    if(nr < 0 || nr >= 101 || nc < 0 || nc >= 101) continue;
-                    
-                    if(!visited[nr][nc] && map[nr][nc] == 1) {
-                        visited[nr][nc] = true;
-                        q.offer(new int[]{nr, nc});
-                    }
+                if(!visited[nr][nc] && map[nr][nc] == 1) {
+                    visited[nr][nc] = true;
+                    q.offer(new int[] {nr, nc, curDist+1});
                 }
             }
-            answer++;
+        }
+        
+        return 0;
+    }
+    
+    void makeRoute(int lc, int lr, int rc, int rr) {
+        for(int i = lr+1; i < rr; i++) {
+            for(int j = lc+1; j < rc; j++) {
+                map[i][j] = 2;
+            }
+        }
+        
+        for(int i = lr; i <= rr; i++) {
+            if(map[i][lc] != 2) map[i][lc] = 1;
+            if(map[i][rc] != 2) map[i][rc] = 1;
+        }
+        
+        for(int i = lc; i <= rc; i++) {
+            if(map[lr][i] != 2) map[lr][i] = 1;
+            if(map[rr][i] != 2) map[rr][i] = 1;
         }
     }
 }
