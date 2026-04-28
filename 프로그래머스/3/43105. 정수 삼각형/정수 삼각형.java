3b68227d7592858992bdaaf3047d6eda1acf15eb
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-04-28 14:27:47 +0900

    [level 3] Title: 정수 삼각형, Time: 12.60 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -2,18 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] triangle) {
-        int answer = 0;
+        int[][] dp = triangle;
+        int answer = Integer.MIN_VALUE;
         
-        for(int i = 1; i < triangle.length; i++) {
-            for(int j = 0; j < triangle[i].length; j++) {
-                if(j == 0) triangle[i][j] += triangle[i-1][j];
-                else if(j == i) triangle[i][j] += triangle[i-1][j-1];
-                else triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
-            
-                answer = Math.max(answer, triangle[i][j]);
+        dp[1][0] = triangle[0][0] + triangle[1][0];
+        dp[1][1] = triangle[0][0] + triangle[1][1];
+        
+        // dp[i][j] : i줄 j번째까지의 누적합
+        for(int i = 2; i < triangle.length; i++) {
+            dp[i][0] = dp[i-1][0] + triangle[i][0];
+            dp[i][triangle[i].length-1] = dp[i-1][triangle[i-1].length-1] + triangle[i][triangle[i].length-1];
+        }
+        
+        for(int i = 2; i < triangle.length; i++) {
+            for(int j = 1; j < triangle[i].length; j++) {
+                if(j > 0 && j < triangle[i].length-1) {
+                    dp[i][j] = Math.max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j];
+                }            
             }
         }
         
+        
+        for(int i = 0; i < dp[dp.length-1].length; i++) {
+            answer = Math.max(answer, dp[dp.length-1][i]);
+        }
+
         return answer;
     }
 }
