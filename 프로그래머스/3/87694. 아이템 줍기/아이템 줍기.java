4394534c043573b4dd8d695f7f510e36093ac3b7
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-05-06 14:40:43 +0900

    [level 3] Title: 아이템 줍기, Time: 1.10 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -2,26 +2,43 @@ import java.util.*;
 
 class Solution {
     int[][] map;
+    boolean[][] visited;
     int[] dr = {-1,1,0,0};
     int[] dc = {0,0,-1,1};
     
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-        int answer = 0;
-        boolean[][] visited = new boolean[102][102];
-        
+
         map = new int[102][102];
+        visited = new boolean[102][102];
+        
         for(int i = 0; i < rectangle.length; i++) {
-            int lc = rectangle[i][0]*2;
-            int lr = rectangle[i][1]*2;
-            int rc = rectangle[i][2]*2;
-            int rr = rectangle[i][3]*2;
+            int leftC = rectangle[i][0] * 2;
+            int leftR = rectangle[i][1] * 2;
+            int rightC = rectangle[i][2] * 2;
+            int rightR = rectangle[i][3] * 2;
             
-            makeRoute(lc, lr, rc , rr);
+            make(leftC, leftR, rightC, rightR);
         }
         
+        return bfs(characterX*2, characterY*2, itemX*2, itemY*2);
+    }
+    
+    void make(int lc, int lr, int rc, int rr) {
+        for(int i = lr; i <= rr; i++) {
+            for(int j = lc; j <= rc; j++) {
+                if(i > lr && i < rr && j > lc && j < rc) {
+                    map[i][j] = -1;
+                } else if(map[i][j] != -1) {
+                    map[i][j] = 1;
+                }
+            }
+        }
+    }
+    
+    int bfs(int sc, int sr, int ec, int er) {
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {characterY*2, characterX*2, 0});
-        visited[characterY*2][characterX*2] = true;
+        q.offer(new int[] {sr, sc, 0});
+        visited[sr][sc] = true;
         
         while(!q.isEmpty()) {
             int[] cur = q.poll();
@@ -29,16 +46,15 @@ class Solution {
             int curC = cur[1];
             int curDist = cur[2];
             
-            if(curR == itemY*2 && curC == itemX*2) {
-                answer = curDist;
-                return answer/2;
+            if(curR == er && curC == ec) {
+                return curDist/2;
             }
             
             for(int d = 0; d < 4; d++) {
                 int nr = curR + dr[d];
                 int nc = curC + dc[d];
                 
-                if(nr < 0 || nr >= 102 || nc < 0 || nc >= 102) continue;
+                if(nr < 0 || nr >= map.length || nc < 0 || nc >= map[0].length) continue;
                 
                 if(!visited[nr][nc] && map[nr][nc] == 1) {
                     visited[nr][nc] = true;
@@ -46,25 +62,6 @@ class Solution {
                 }
             }
         }
-        
         return 0;
-    }
-    
-    void makeRoute(int lc, int lr, int rc, int rr) {
-        for(int i = lr+1; i < rr; i++) {
-            for(int j = lc+1; j < rc; j++) {
-                map[i][j] = 2;
-            }
-        }
-        
-        for(int i = lr; i <= rr; i++) {
-            if(map[i][lc] != 2) map[i][lc] = 1;
-            if(map[i][rc] != 2) map[i][rc] = 1;
-        }
-        
-        for(int i = lc; i <= rc; i++) {
-            if(map[lr][i] != 2) map[lr][i] = 1;
-            if(map[rr][i] != 2) map[rr][i] = 1;
-        }
     }
 }
