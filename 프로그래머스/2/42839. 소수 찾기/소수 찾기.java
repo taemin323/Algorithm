fb69a9aef7d139714fe7380d0f7f9de6795b6b33
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-06-16 10:21:57 +0900

    [level 2] Title: 소수 찾기, Time: 10.75 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,34 +1,35 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer> set = new HashSet<>();
     public int solution(String numbers) {
         int answer = 0;
         
-        perm("", numbers);
+        Set<Integer> set = new HashSet<>();
         
-        for(int s : set) {
-            if(check(s)) answer++;
+        perm("", numbers, set);
+        
+        for(int num : set) {
+            if(isPrime(num)) answer++;
         }
         
         return answer;
     }
     
-    boolean check(int num) {
-        if(num <= 1) return false;
+    void perm(String prev, String numbers, Set<Integer> set) {
+        if(!prev.equals("")) set.add(Integer.valueOf(prev));
         
-        for(int i = 2 ; i <= Math.sqrt(num); i++) {
+        for(int i = 0; i < numbers.length(); i++) {
+            perm(prev + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i+1, numbers.length()), set);
+        }
+    }
+    
+    boolean isPrime(int num) {
+        if(num == 0 || num == 1) return false;
+        
+        for(int i = 2; i <= Math.sqrt(num); i++) {
             if(num % i == 0) return false;
         }
         
         return true;
-    }
-    
-    void perm(String prev, String numbers) {
-        if(!prev.equals("")) set.add(Integer.valueOf(prev));
-        
-        for(int i = 0; i < numbers.length(); i++) {
-            perm(prev + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i+1, numbers.length()));
-        }
     }
 }
