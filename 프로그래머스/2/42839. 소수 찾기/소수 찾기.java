b764ae66fed9f3453d9f0da79110cb0629c8d96d
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-06-23 10:55:38 +0900

    [level 2] Title: 소수 찾기, Time: 12.22 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,35 +1,39 @@
 import java.util.*;
+/**
+* numbers로 만들 수 있는 모든 숫자를 set에 저장
+* prev 문자열에 현재 문자를 더하고 numbers에서는 그 문자를 뺀걸 넘김
+* 만들어진 set에서 소수인 것들만 카운트
+*/
 
 class Solution {
+    Set<Integer> set = new HashSet<>();
+    
     public int solution(String numbers) {
         int answer = 0;
         
-        Set<Integer> set = new HashSet<>();
+        perm("", numbers);
         
-        perm("", numbers, set);
-        
-        for(int num : set) {
-            if(isPrime(num)) answer++;
+        for(int s : set) {
+            if(isPrime(s)) answer++;//소수라면 answer에 추가
         }
         
         return answer;
     }
     
-    void perm(String prev, String numbers, Set<Integer> set) {
-        if(!prev.equals("")) set.add(Integer.valueOf(prev));
+    void perm(String prev, String numbers) {
+        if(!prev.equals("")) set.add(Integer.valueOf(prev));//만들어진 숫자 set에 저장
         
         for(int i = 0; i < numbers.length(); i++) {
-            perm(prev + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i+1, numbers.length()), set);
+            perm(prev+numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i+1, numbers.length()));
         }
     }
     
-    boolean isPrime(int num) {
-        if(num == 0 || num == 1) return false;
+    boolean isPrime(int n) {
+        if(n <= 1) return false;
         
-        for(int i = 2; i <= Math.sqrt(num); i++) {
-            if(num % i == 0) return false;
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) return false;
         }
-        
         return true;
     }
 }
