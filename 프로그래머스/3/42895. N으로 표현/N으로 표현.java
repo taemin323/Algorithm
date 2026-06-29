e5e4c7375ef1e544f1d80ed6d7451d7809413180
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-06-29 22:38:43 +0900

    [level 3] Title: N으로 표현, Time: 18.25 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,34 +1,40 @@
 import java.util.*;
+/**
+* 최솟값이 8보다 크면 어차피 -1임
+* 1부터 8개까지 numbers를 만들 수 있는지 체크?
+* 1개일 땐 어차피 사칙연상이 안되니 바로 비교
+* 2개부터는 n+n, n/n, n*n, n-n이 됨.
+* 3개부터는 사칙연산과 괄호를 생각해야됨.
+* 3개짜리 = 1개짜리 (연산) 2개짜리 또는 2개짜리 (연산) 1개짜리
+* 즉 k개 = 1개 op (k-1)개
+*       = 2개 op (k-2)개
+*/
 
 class Solution {
     public int solution(int N, int number) {
         
-        // dp[i] : N을 i개 사용해서 만들 수 있는 수들의 집합
-        List<Set<Integer>> dp = new ArrayList<>();
-        for(int i = 0; i <= 8; i++) {
-            dp.add(new HashSet<>());
-        }
+        //dp[i] = N을 i개 써서 만들 수 있는 수의 집합
+        Set<Integer>[] dp = new HashSet[9];
         
         String str = "";
         for(int i = 1; i <= 8; i++) {
             str += N;
-            dp.get(i).add(Integer.parseInt(str));
-        }
-        
-        for(int i = 1; i <= 8; i++) {
-            for(int j = 1; j < i; j++) {
-                for(int a : dp.get(j)) {
-                    for(int b : dp.get(i-j)) {
-                        dp.get(i).add(a+b);
-                        dp.get(i).add(a-b);
-                        dp.get(i).add(a*b);
-                        if(b != 0) dp.get(i).add(a/b);
+            dp[i] = new HashSet<>();
+            dp[i].add(Integer.parseInt(str));
+            
+            for(int k = 1; k < i; k++) {
+                for(int a : dp[k]) {
+                    for(int b : dp[i-k]) {
+                        dp[i].add(a - b);
+                        dp[i].add(a + b);
+                        dp[i].add(a * b);
+                        if(b != 0) dp[i].add(a / b);    
                     }
                 }
             }
-            if(dp.get(i).contains(number)) return i;
+            
+            if(dp[i].contains(number)) return i;
         }
-        
         return -1;
     }
 }
