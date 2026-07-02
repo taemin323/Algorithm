793e94aa78b3ee9a96b30b8a8a20aef4b855ae65
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-07-02 13:00:29 +0900

    [level 3] Title: 등굣길, Time: 0.36 ms, Memory: 57.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,38 +1,42 @@
 import java.util.*;
 /**
-* 1 <= m, n <= 10
-* 0 <= puddles.length <= 10
+* dp[i][j] : 출발점에서부터 (i,j)까지 도달하는 최단경로의 수 저장
+* 오른쪽, 아래쪽으로만 이동이 가능하니, dp[i][j] = dp[i-1][j] + dp[i][j-1]
 */
 
 class Solution {
-    private static int[] dr = {1,0};
-    private static int[] dc = {0,1};
-    private static final int MOD = 1000000007;
-    
     public int solution(int m, int n, int[][] puddles) {
+        int num = 1000000007;
         
-        int[][]map = new int[n+1][m+1];
+        int[][] dp = new int[n+1][m+1];
+        boolean[][] water = new boolean[n+1][m+1];
         for(int i = 0; i < puddles.length; i++) {
             int r = puddles[i][1];
             int c = puddles[i][0];
-            map[r][c] = 1;
+            
+            water[r][c] = true;
         }
         
-        long[][] dp = new long[n+1][m+1];
+        
         dp[1][1] = 1;
         
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= m; j++){
-                if(i == 1 && j == 1) continue;
-                
-                if(map[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % MOD;
-                }
-            }
+        //맨위, 맨왼쪽은 무조건 최단경로의 수가 1개임
+        for(int i = 2; i <= m; i++) {
+            if(water[1][i]) break;
+            
+            dp[1][i] = 1;
+        }
+        for(int i = 2; i <= n; i++) {
+            if(water[i][1]) break;
+            
+            dp[i][1] = 1;
         }
         
-        return (int) dp[n][m];
+        for(int i = 2; i <= n; i++) {
+            for(int j = 2; j <= m; j++) {
+                if(!water[i][j]) dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % num;
+            }
+        }
+        return dp[n][m];
     }
 }
