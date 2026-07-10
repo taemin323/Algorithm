aaa55fc9c08bffaaa76a00e8a6fb075c3b1179f1
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-07-10 13:03:47 +0900

    [level 2] Title: 오픈채팅방, Time: 117.37 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -2,35 +2,33 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] record) {
-        //Map으로 id, 닉네임 관리
+        int n = record.length;
+        
+        
         Map<String, String> map = new HashMap<>();
+        String[] type = new String[n];
+        String[] id = new String[n];
+        List<String> result = new ArrayList<>();
         
-        String[] type = new String[record.length];
-        String[] id = new String[record.length];
-        
-        for(int i = 0; i < record.length; i++) {
+        for(int i = 0; i < n; i++) {
             String[] parts = record[i].split(" ");
-            
             type[i] = parts[0];
             id[i] = parts[1];
             
-            if(type[i].equals("Enter") || type[i].equals("Change")) {
-                map.put(id[i], parts[2]);
-            } 
+            if(type[i].equals("Leave")) continue;
+            
+            map.put(parts[1], parts[2]);
         }
         
-        List<String> result = new ArrayList<>();
-        
-        for(int i = 0; i < record.length; i++) {
+        for(int i = 0; i < n; i++) {
+            if(type[i].equals("Change")) continue;
+            
             if(type[i].equals("Enter")) {
                 result.add(map.get(id[i]) + "님이 들어왔습니다.");
-            }
-            
-            if(type[i].equals("Leave")) {
+            } else {
                 result.add(map.get(id[i]) + "님이 나갔습니다.");
             }
         }
-        
-        return result.toArray(new String[0]);
+        return result.stream().toArray(String[]::new);
     }
 }
