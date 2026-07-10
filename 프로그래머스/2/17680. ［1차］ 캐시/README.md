efe82c70c5bc6f611812f03d64388fdb2de56e2f
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-07-10 15:33:50 +0900

    [level 2] Title: [1차] 캐시, Time: 24.17 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
 
 ### 성능 요약
 
-메모리: 120 MB, 시간: 23.87 ms
+메모리: 118 MB, 시간: 24.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 08일 12:03:05
+2026년 07월 10일 15:33:46
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,30 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int cacheSize, String[] cities) {
-        int answer = 0;
-        int n = cities.length;
-        List<String> cache = new ArrayList<>();
-        
         if(cacheSize == 0) return cities.length * 5;
         
-        for(int i = 0; i < n; i++) {
-            cities[i] = cities[i].toLowerCase();
+        List<String> cach = new ArrayList<>();
+        int answer = 0;
+        
+        
+        for(int i = 0; i < cities.length; i++) {
+            String city = cities[i].toUpperCase();
             
-            String city = cities[i];
-            
-            // 캐시 안에 현재 도시가 있는 경우
-            if(cache.contains(city)) {
-                cache.remove(city);
-                cache.add(city);
-                answer += 1;
-            } else {// 캐시 안에 현재 도시가 없는 경우
-                //캐시가 꽉 차 있다면
-                if(cache.size() >= cacheSize) {
-                    cache.remove(0);
+            if(cach.size() >= cacheSize) {
+                if(cach.contains(city)) {
+                    cach.remove(city);
+                    cach.add(city);
+                    answer += 1;
+                } else {
+                    cach.remove(0);
+                    cach.add(city);
+                    answer += 5;
                 }
-                cache.add(city);
-                answer += 5;
+            } else {
+                if(cach.contains(city)) {
+                    cach.remove(city);
+                    cach.add(city);
+                    answer += 1;
+                } else {
+                    cach.add(city);
+                    answer += 5;;
+                }
+                
             }
+            
         }
         
         return answer;
