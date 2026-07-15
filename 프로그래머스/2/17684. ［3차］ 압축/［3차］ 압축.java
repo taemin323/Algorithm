e43a1b83e7bfa0ce54fa9610379f84bb794f23f1
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-07-15 13:35:39 +0900

    [level 2] Title: [3차] 압축, Time: 9.41 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,34 +1,43 @@
 import java.util.*;
+/**
+* 사전 -> Map으로 관리.
+* i = 0 ~ msg.length-1까지 while
+* while문 내부에서 k, ka, kao를 순서대로 사전에 있는지 체크
+* 없는 문자열이 나오면 그걸 사전에 저장하고 그 이전 즉 있는 문자열을 그대로 출력 list에 담고
+* i는 그 문자열만큼 건너뛴 위치로 이동해서 다시 while문 내부 반복
+*/
 
 class Solution {
     public int[] solution(String msg) {
-        List<Integer> result = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
+        Map<String, Integer> dict = new HashMap<>();
         
-        Map<String, Integer> map = new HashMap<>();
         int dictSize = 1;
         for(char c = 'A'; c <= 'Z'; c++) {
-            map.put(String.valueOf(c), dictSize++);
+            dict.put(String.valueOf(c), dictSize++);
         }
         
         int i = 0;
         while(i < msg.length()) {
             String w = "";
+            
             int j = i;
-            //현재 위치 i에서 시작하는 prefix를 한 글자씩 늘려가며 체크
-            while(j < msg.length() && map.containsKey(w + msg.charAt(j))) {
+            while(j < msg.length() && dict.containsKey(w+msg.charAt(j))) {
                 w += msg.charAt(j);
                 j++;
             }
             
-            // w는 사전에 있는 가장 긴 prefix
-            result.add(map.get(w));
+            //사전에 새로 저장
             if(j < msg.length()) {
-                map.put(w + msg.charAt(j), map.size()+1);//새 항목 추가
+                dict.put(w+msg.charAt(j), dictSize++);
             }
+            
+            //정답에 출력값 추가
+            answer.add(dict.get(w));
             
             i += w.length();
         }
         
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
