a6fe8cb30575283c081331ca12f20c9b29d8c092
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-07-17 11:07:23 +0900

    [level 2] Title: [3차] 압축, Time: 8.78 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,15 +1,8 @@
 import java.util.*;
-/**
-* 사전 -> Map으로 관리.
-* i = 0 ~ msg.length-1까지 while
-* while문 내부에서 k, ka, kao를 순서대로 사전에 있는지 체크
-* 없는 문자열이 나오면 그걸 사전에 저장하고 그 이전 즉 있는 문자열을 그대로 출력 list에 담고
-* i는 그 문자열만큼 건너뛴 위치로 이동해서 다시 while문 내부 반복
-*/
 
 class Solution {
     public int[] solution(String msg) {
-        List<Integer> answer = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         Map<String, Integer> dict = new HashMap<>();
         
         int dictSize = 1;
@@ -22,22 +15,21 @@ class Solution {
             String w = "";
             
             int j = i;
-            while(j < msg.length() && dict.containsKey(w+msg.charAt(j))) {
+            while(j < msg.length() && dict.containsKey(w + msg.charAt(j))) {
+                
                 w += msg.charAt(j);
                 j++;
             }
             
-            //사전에 새로 저장
-            if(j < msg.length()) {
-                dict.put(w+msg.charAt(j), dictSize++);
-            }
+            result.add(dict.get(w));
             
-            //정답에 출력값 추가
-            answer.add(dict.get(w));
+            if(j < msg.length()) {
+                dict.put(w+msg.charAt(j), dict.size()+1);
+            }
             
             i += w.length();
         }
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return result.stream().mapToInt(Integer::intValue).toArray();
     }
 }
