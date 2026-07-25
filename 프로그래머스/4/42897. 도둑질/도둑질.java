33413a5ddbd1e7e931e8d8e1671bbdfc3db4881c
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-07-25 17:01:28 +0900

    [level 4] Title: 도둑질, Time: 23.09 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,28 +1,27 @@
 import java.util.*;
-/**
-* 첫 번째 집을 가냐 안가냐로 상황을 나눠서 dp를 진행.
-*/
 
 class Solution {
     public int solution(int[] money) {
-        //첫 번째 집 가는 경우
-        int[] dp1 = new int[money.length];
+        int n = money.length;
+        
+        //첫번째 털었을 경우 -> 마지막 집 도달 x
+        int[] dp1 = new int[n];
         dp1[0] = money[0];
         dp1[1] = money[0];
         int max1 = dp1[0];
         
-        for(int i = 2; i < money.length-1; i++) {
-            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + money[i]);
+        for(int i = 2; i < n-1; i++) {
+            dp1[i] = Math.max(dp1[i-1], money[i] + dp1[i-2]);
             max1 = Math.max(max1, dp1[i]);
         }
         
-        //첫 번째 집 가지 않는 경우
-        int[] dp2 = new int[money.length];
+        //첫번째 안털었을 경우 -> 마지막 집 갈 수 있음.
+        int[] dp2 = new int[n];
         dp2[1] = money[1];
         int max2 = dp2[1];
         
-        for(int i = 2; i < money.length; i++) {
-            dp2[i] = Math.max(dp2[i-1], dp2[i-2] + money[i]);
+        for(int i = 2; i < n; i++) {
+            dp2[i] = Math.max(dp2[i-1], money[i] + dp2[i-2]);
             max2 = Math.max(max2, dp2[i]);
         }
         
