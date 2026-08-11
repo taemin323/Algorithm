caa65af712abed3eb6fa2eda77b345358cd48420
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-08-11 10:34:36 +0900

    [level 3] Title: 가장 먼 노드, Time: 20.20 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,63 +1,44 @@
 import java.util.*;
-/**
-* 25분 -> 실패
-* 가장 멀리 떨어지 노드란 최단경로로 이동했을 때 간선의 개수
-* 간선의 가중치는 없음.
-* 출발지도 고정. 다익스트라로 풀면 됨.
-* max 구하고 그 max와 일치하는 dist의 인덱스 카운팅하기.
-*/
 
 class Solution {
-    int answer = 0;
     public int solution(int n, int[][] edge) {
         List<Integer>[] graph = new ArrayList[n+1];
-        
-        for(int i = 0; i < n+1; i++) {
+        for(int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
-        
-        for(int i = 0; i < edge.length; i++) {
-            int from = edge[i][0];
-            int to = edge[i][1];
-            
-            graph[from].add(to);
-            graph[to].add(from);
+        for(int[] e : edge) {
+            graph[e[0]].add(e[1]);
+            graph[e[1]].add(e[0]);
         }
         
-        List<int[]> result = dijkstra(1, n, graph);
+        return bfs(1, n, graph);
+    }
+    
+    int bfs(int start, int n, List<Integer>[] graph) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[n+1];
         
-        Collections.sort(result, (a,b) -> b[1] - a[1]);
-        for(int i = 0; i < result.size(); i++) {
-            if(result.get(0)[1] == result.get(i)[1]) answer++;
+        q.add(start);
+        visited[start] = true;
+        
+        int answer = 0;
+        
+        while(!q.isEmpty()) {
+            int qSize = q.size();
+            answer = qSize;
+            
+            for(int i = 0; i < qSize; i++) {
+                int cur = q.poll();
+                
+                for(int next : graph[cur]) {
+                    if(!visited[next]) {
+                        visited[next] = true;
+                        q.add(next);
+                    }
+                }
+            }
         }
         
         return answer;
-    }
-    
-    List<int[]> dijkstra(int start, int n, List<Integer>[] graph) {
-        List<int[]> result = new ArrayList<>();
-        boolean[] visited = new boolean[n+1];
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[1] - b[1]);
-        pq.add(new int[] {start, 0});
-        visited[start] = true;
-        
-        while(!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curNode = cur[0];
-            int curDist = cur[1];
-            
-            result.add(new int[] {curNode, curDist});
-                        
-            for(int i : graph[curNode]) {
-                if(!visited[i]) {
-                    visited[i] = true;
-                    pq.add(new int[] {i, curDist+1});
-                }
-            }
-            
-        }
-        
-        return result;
     }
 }
