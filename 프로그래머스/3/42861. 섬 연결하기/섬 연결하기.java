9cd46de7619ccddd54a7c51b8617ef47842dd0cb
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-08-14 10:37:08 +0900

    [level 3] Title: 섬 연결하기, Time: 4.28 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,48 +1,53 @@
 import java.util.*;
+/**
+* 최소 신장 트리(MST) 문제
+* 크루스칼로 구해보자
+* 크루스칼 알고리즘에서는 인접리스트 필요없음.
+* 모든 간선을 가중치 오름차순으로 정렬
+* 가중치가 작은 간선부터 하나씩 확인하면서, 사이클을 만들지 않으면 선택(union-find로 사이클 판정하자)
+* 간선을 n-1개 선택하면 종료
+*/
 
 class Solution {
-    
-    private static int[] parent;
+    int[] parent;
     
     public int solution(int n, int[][] costs) {
-        parent = new int[n];
+        int answer = 0;
         
-        for(int i = 0; i < n; i++) {
+        parent = new int[n+1];
+        
+        for(int i = 1; i <= n; i++) {
             parent[i] = i;
         }
         
         Arrays.sort(costs, (a,b) -> a[2] - b[2]);
         
-        int answer = 0;
         int cnt = 0;
-        
-        for(int[] cost : costs) {
-            int a = cost[0];
-            int b = cost[1];
-            int price = cost[2];
+        for(int i = 0; i < costs.length; i++) {
+            int from = costs[i][0];
+            int to = costs[i][1];
+            int cost = costs[i][2];
             
-            if(find(a) != find(b)) {
-                union(a, b);
-                answer += price;
+            if(find(from) != find(to)) {
+                union(from, to);
+                answer += cost;
                 cnt++;
-                
-                if(cnt == n - 1) break;
             }
+            
+            if(cnt == n-1) break;
         }
+        
         return answer;
-    }
-    
-    int find(int x) {
-        if(parent[x] == x) return x; 
-        return parent[x] = find(parent[x]);
     }
     
     void union(int a, int b) {
         int pa = parent[a];
         int pb = parent[b];
-        
-        if(pa != pb) {
-            parent[pb] = pa;
-        }
+        if(pa != pb) parent[pb] = pa;
+    }
+    
+    int find(int a) {
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
     }
 }
