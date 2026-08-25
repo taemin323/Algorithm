ae81c6549f1b416e46805b169950401bc46f0788
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-08-25 13:55:54 +0900

    [level 4] Title: 사칙연산, Time: 7.45 ms, Memory: 57.2 MB -BaekjoonHub

--- 프로그래머스/4/1843. 사칙연산/사칙연산.java
+++ 프로그래머스/4/1843. 사칙연산/사칙연산.java
@@ -1,4 +1,8 @@
 import java.util.*;
+/**
+* dpMax[i][j] : i부터 j까지의 연산했을 때 최댓값
+* dpMin[i][j] : i부터 j까지의 연산했을 때 최솟값
+*/
 
 class Solution {
     public int solution(String arr[]) {
@@ -21,13 +25,13 @@ class Solution {
         }
         
         for(int len = 2; len <= n; len++) {
-            for(int i = 0; i <= n-len; i++) {
-                int j = i+len-1;
+            for(int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
                 
                 dpMax[i][j] = Integer.MIN_VALUE;
                 dpMin[i][j] = Integer.MAX_VALUE;
                 
-                for(int k = i; k <= j-1; k++) {
+                for(int k = i; k < j; k++) {
                     if(ops[k] == '+') {
                         dpMax[i][j] = Math.max(dpMax[i][j], dpMax[i][k] + dpMax[k+1][j]);
                         dpMin[i][j] = Math.min(dpMin[i][j], dpMin[i][k] + dpMin[k+1][j]);
