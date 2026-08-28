2b9ebf2cc946c1f3c334f4bac2995417eb5c7396
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-08-28 13:59:21 +0900

    [level 3] Title: 섬 연결하기, Time: 2.05 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,26 +1,19 @@
 import java.util.*;
 /**
-* 최소 신장 트리(MST) 문제
-* 크루스칼로 구해보자
-* 크루스칼 알고리즘에서는 인접리스트 필요없음.
-* 모든 간선을 가중치 오름차순으로 정렬
-* 가중치가 작은 간선부터 하나씩 확인하면서, 사이클을 만들지 않으면 선택(union-find로 사이클 판정하자)
-* 간선을 n-1개 선택하면 종료
+* 간선의 개수는 n-1
 */
 
 class Solution {
     int[] parent;
-    
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        parent = new int[n+1];
+        Arrays.sort(costs, (a,b) -> a[2] - b[2]);
         
+        parent = new int[n+1];
         for(int i = 1; i <= n; i++) {
             parent[i] = i;
         }
-        
-        Arrays.sort(costs, (a,b) -> a[2] - b[2]);
         
         int cnt = 0;
         for(int i = 0; i < costs.length; i++) {
@@ -40,14 +33,14 @@ class Solution {
         return answer;
     }
     
-    void union(int a, int b) {
-        int pa = parent[a];
-        int pb = parent[b];
-        if(pa != pb) parent[pb] = pa;
+    int find(int x) {
+        if(parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
     }
     
-    int find(int a) {
-        if(parent[a] == a) return a;
-        return parent[a] = find(parent[a]);
+    void union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
+        if(rootA != rootB) parent[rootB] = rootA;
     }
 }
