6f3add7dd9a3d40356c96b06e6615f8dea169f0e
Taemin Kim <155932839+taemin323@users.noreply.github.com>
2026-09-11 14:48:35 +0900

    [level 4] Title: 징검다리, Time: 18.32 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,4 +1,10 @@
 import java.util.*;
+/**
+* 일단 바위 배열을 정렬 때려야 됨.
+* 바위를 n개 제거했을 경우 중에서 거리의 최솟값을 구하고 그 중 제일 큰 값 반환.
+* 완전탐색은 일단 무조건 시간초과.
+* 
+*/
 
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
@@ -6,11 +12,11 @@ class Solution {
         
         Arrays.sort(rocks);
         
-        int left = 1;
+        int left = 0;
         int right = distance;
         
-        while (left <= right) {
-            int mid = (left + right)/2;
+        while(left <= right) {
+            int mid = (right - left)/2 + left;
             
             if(removedRocks(rocks, mid, distance) <= n) {
                 answer = mid;
@@ -19,10 +25,11 @@ class Solution {
                 right = mid - 1;
             }
         }
+    
         return answer;
     }
     
-    public int removedRocks(int[] rocks, int mid, int distance) {
+    int removedRocks(int[] rocks, int mid, int distance) {
         int removedCnt = 0;
         
         int start = 0;
@@ -36,8 +43,7 @@ class Solution {
             start = rocks[i];
         }
         
-        if(end- start < mid) removedCnt++;
-        
+        if(end - start < mid) removedCnt++;
         return removedCnt;
     }
 }
